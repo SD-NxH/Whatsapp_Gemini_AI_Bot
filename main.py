73bde3f7713cca5721e65c6978ff06b2ded95287
SD-NxH <128203074+SD-NxH@users.noreply.github.com>
2025-06-05 12:18:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ wa_token=os.environ.get("WA_TOKEN")
 genai.configure(api_key=os.environ.get("GEN_API"))
 phone_id=os.environ.get("PHONE_ID")
 phone=os.environ.get("PHONE_NUMBER")
-name="Eric Johnson" #The bot will consider this person as its owner or creator
+name="Eric J" #The bot will consider this person as its owner or creator
 bot_name="NxMeal" #This will be the name of your bot, eg: "Hello I am Astro Bot"
 model_name="gemini-1.5-flash-latest" #Switch to "gemini-1.0-pro" or any free model, if "gemini-1.5-flash" becomes paid in future.
 
